8da910b15690ab7e00dc1bc846563ac7fded9e28
manoj140985 <kottemanoj14098510@gmail.com>
2025-10-08 09:13:38 +0530

    Add files via upload

---
+++ WEEK7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "Java", author: "Mark", price: 4909, availability: "in stock" },
+  { title: "C", author: "Mask", price: 699, availability: "in stock" },
+  { title: "C++", author: " Nolan", price: 59999, availability: "out of stock" },
+  { title: "Rust", author: "Clark", price: 4500, availability: "in stock" }
+];
